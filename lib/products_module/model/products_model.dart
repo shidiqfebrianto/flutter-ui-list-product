1f23f4ca7d8d0f50915f5ae4a223511a266742be
shidiq febrianto <shidiq.febrianto@binus.ac.id>
2022-07-07 02:10:55 +0700

    Create model "products_model"

---
+++ lib/products_module/model/products_model.dart
@@ -0,0 +1,100 @@
+// To parse this JSON data, do
+//
+//     final productsModel = productsModelFromJson(jsonString);
+
+import 'dart:convert';
+
+List<ProductsModel> productsModelFromJson(String str) =>
+    List<ProductsModel>.from(
+        json.decode(str).map((x) => ProductsModel.fromJson(x)));
+
+String productsModelToJson(List<ProductsModel> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class ProductsModel {
+  ProductsModel({
+    this.id,
+    this.title,
+    this.price,
+    this.description,
+    this.category,
+    this.image,
+    this.rating,
+  });
+
+  int? id;
+  String? title;
+  double? price;
+  String? description;
+  String? category;
+  String? image;
+  Rating? rating;
+
+  ProductsModel copyWith({
+    int? id,
+    String? title,
+    double? price,
+    String? description,
+    String? category,
+    String? image,
+    Rating? rating,
+  }) =>
+      ProductsModel(
+        id: id ?? this.id,
+        title: title ?? this.title,
+        price: price ?? this.price,
+        description: description ?? this.description,
+        category: category ?? this.category,
+        image: image ?? this.image,
+        rating: rating ?? this.rating,
+      );
+
+  factory ProductsModel.fromJson(Map<String, dynamic> json) => ProductsModel(
+        id: json["id"],
+        title: json["title"],
+        price: json["price"] == null ? null : json["price"]!.toDouble(),
+        description: json["description"],
+        category: json["category"],
+        image: json["image"],
+        rating: json["rating"] == null ? null : Rating.fromJson(json["rating"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "title": title,
+        "price": price,
+        "description": description,
+        "category": category,
+        "image": image,
+        "rating": rating == null ? null : rating!.toJson(),
+      };
+}
+
+class Rating {
+  Rating({
+    this.rate,
+    this.count,
+  });
+
+  double? rate;
+  int? count;
+
+  Rating copyWith({
+    double? rate,
+    int? count,
+  }) =>
+      Rating(
+        rate: rate ?? this.rate,
+        count: count ?? this.count,
+      );
+
+  factory Rating.fromJson(Map<String, dynamic> json) => Rating(
+        rate: json["rate"] == null ? null : json["rate"].toDouble(),
+        count: json["count"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "rate": rate,
+        "count": count,
+      };
+}
