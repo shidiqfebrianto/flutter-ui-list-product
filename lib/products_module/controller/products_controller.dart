30e4a9d91aea606321c5625bf34c898864f4dfd0
shidiq febrianto <shidiq.febrianto@binus.ac.id>
2022-07-08 00:11:11 +0700

    Update Product Controller
    - Create function for detection device
    - Create offsetCount for manage list product
    - Create scrollController for detection scroll position
    - Set offsetCount base on device detection
    - Validation when scroll add data until data max

--- lib/products_module/controller/products_controller.dart
+++ lib/products_module/controller/products_controller.dart
@@ -1,33 +1,65 @@
 import 'dart:convert';
 import 'dart:developer';
 
-import 'package:flutter_ui_list_product/network_services/network_request.dart';
-import 'package:flutter_ui_list_product/products_module/model/products_model.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
+
+import '../../network_services/network_request.dart';
+import '../model/products_model.dart';
 
 class ProductsController extends GetxController {
   var productsList = <ProductsModel>[].obs;
+  var offsetCount = 0.obs;
+  var isMobile = false.obs;
+  var isTablet = false.obs;
+  var isDesktop = false.obs;
+  final scrollController = ScrollController();
+
   final networkRequest = NetworkRequest();
   final defaultImage =
       'https://avatars.githubusercontent.com/u/14101776?s=280&v=4';
-  final apiProducts = 'https://fakestoreapi.com/products';
+  final apiProducts = '/products';
 
   void fetchProductsList() async {
-    networkRequest
-        .get(apiProducts, contentType: 'application/json')
-        .then((value) {
-      log(json.encode(value.body));
+    networkRequest.get(apiProducts).then((value) {
+      log('${value.statusCode}: ${value.statusText}',
+          name: '[status] fetchProductsList');
+      log(json.encode(value.body), name: '[body] fetchProductsList');
+
       productsList.addAll(productsModelFromJson(json.encode(value.body)));
-      // List list = json.encode(value.body);
-      // productsList.addAll(list.map((e) => ProductsModel.fromJson(e)).toList());
     }).catchError((onError) {
-      log(onError.toString(), error: 'catchError');
+      log(onError.toString(), error: onError, name: 'catchError');
     });
   }
 
   @override
   void onInit() {
+    if (GetPlatform.isDesktop) {
+      //isDesktop
+      offsetCount.value = 16;
+      isDesktop.value = true;
+    } else {
+      //isMobile or isTablet
+      offsetCount.value = 4;
+      if (Get.size.shortestSide < 600) {
+        //isMobile
+        isMobile.value = true;
+      } else {
+        //isTablet
+        isTablet.value = true;
+      }
+    }
+    networkRequest.onInit();
     fetchProductsList();
+    scrollController.addListener(() {
+      if (scrollController.position.pixels ==
+              scrollController.position.maxScrollExtent &&
+          offsetCount < productsList.length) {
+        offsetCount = offsetCount + 1;
+        log('total on screen => $offsetCount/${productsList.length}',
+            name: 'show new data');
+      }
+    });
     super.onInit();
   }
 }
