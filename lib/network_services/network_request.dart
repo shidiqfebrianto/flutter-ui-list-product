1c10e6f8fce7d903f377439579fdc420876ff7ea
shidiq febrianto <shidiq.febrianto@binus.ac.id>
2022-07-08 00:07:34 +0700

    Setup NetworkRequest
    - Set baseUrl
    - Set defaultContentType
    - Set userAgent
    - Set timeout

--- lib/network_services/network_request.dart
+++ lib/network_services/network_request.dart
@@ -1,5 +1,27 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:get/get.dart';
 
 class NetworkRequest extends GetConnect {
-  // Future<Response> getJsonData(String url) => get(url);
+  @override
+  void onInit() {
+    httpClient.baseUrl = "https://fakestoreapi.com";
+    httpClient.defaultContentType = "application/json";
+    httpClient.userAgent = "PostmanRuntime/7.29.0";
+    httpClient.timeout = const Duration(seconds: 10);
+
+    httpClient.addRequestModifier<dynamic>((request) async {
+      log(json.encode(request.headers),
+          name: '${request.url.host}${request.url.path}');
+      return request;
+    });
+    // var headers = {'Authorization': "Bearer $token"};
+    // httpClient.addAuthenticator((request) async {
+    //   request.headers.addAll(headers);
+    //   return request;
+    // });
+
+    super.onInit();
+  }
 }
