7d4ea19c0f4f2eb6f24f1ae8ab67b55da9c9aa1f
shidiq febrianto <shidiq.febrianto@binus.ac.id>
2022-07-07 13:52:09 +0700

    Create CustomColor
    For base of color in this apps

---
+++ lib/utils/custom_color.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+import 'hex_color.dart';
+
+class CustomColor {
+  static Color black = HexColor.fromHex('#000000');
+  static Color white = HexColor.fromHex('#ffffff');
+  static Color gray = HexColor.fromHex('#C4C4C4');
+}
